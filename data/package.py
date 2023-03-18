8dec192a140f90352d34da4abffcc67bb8d5ab19
Nauman Mustafa <nauman.mustafa.x@gmail.com>
2023-03-18 12:22:14 +0500

    add packaging script

--- data/package.py
+++ data/package.py
@@ -1,25 +1,36 @@
-import glob
+# buuild and push to huggingface datasets
+import json
+import random
+from pathlib import Path
+
+import pandas as pd
 from PIL import Image
-from tqdm import tqdm
-from zipfile import ZipFile
-from collections import Counter
+from datasets import load_dataset
 
-base_path = "../icon-data/*.png"
-files = glob.glob(base_path, recursive=True)
-files.sort()
+base_path = Path("../icon-data-2")
+files = list(base_path.glob("*.png"))
+# remove files with non-one aspect ratio
+img_files = []
+tags_info = []
+for fn in files:
+    im = Image.open(fn)
+    w, h = im.size
+    if w == h:
+        img_files.append(str(fn))
+        with open(fn.with_suffix(".json"), "r") as f:
+            tags = json.load(f)
+        random.shuffle(tags)
+        tags_info.append("bw-icon:" + ", ".join(tags))
+    im.close()
 
-with open("dataset.zip", "wb") as f:
-    with ZipFile(f, "w") as zip:
-        sizes = []
-        for i, file in enumerate(tqdm(files)):
-            im = Image.open(file)
-            sizes.append(im.size)
-            if im.size == (512, 512):
-                with zip.open(f"icons/{i:05d}.png", "w") as zf:
-                    im = im.resize((256, 256), Image.LANCZOS).convert("RGBA")
-                    new = Image.new("RGB", (256, 256), (255, 255, 255))
-                    new.paste(im, (0, 0), im)
-                    new.save(zf, format="PNG")
-            im.close()
+df = pd.DataFrame({"image": img_files, "tags": tags_info})
+df.to_csv("icon_data.csv", index=False)
 
-print(Counter(sizes).most_common(10))
+# build and push to huggingface datasets
+dataset = load_dataset("csv", data_files="icon_data.csv")
+# also load the images
+dataset = dataset.map(
+    lambda x: {"image": Image.open(x["image"]).resize((128, 128))},
+    remove_columns=["image"],
+)
+dataset.push_to_hub("naxalpha/stable-icons-128")
